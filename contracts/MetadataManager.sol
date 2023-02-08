93cc7ff68521162e293141e36d482fe93b0b1e48
Steven Enamakel <enamakel@mahadao.com>
2023-02-08 20:24:14 +0530

    added nft metadata

--- contracts/MetadataManager.sol
+++ contracts/MetadataManager.sol
@@ -7,8 +7,10 @@ import {IMetadataManager} from "./interfaces/IMetadataManager.sol";
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import {VersionedInitializable} from "./proxy/VersionedInitializable.sol";
 import {INFTLocker} from "./interfaces/INFTLocker.sol";
+import {ITokenURIGenerator} from "./interfaces/ITokenURIGenerator.sol";
 
 contract Metadata is
+    ITokenURIGenerator,
     IMetadataManager,
     AccessControlEnumerable,
     VersionedInitializable
@@ -98,6 +100,45 @@ contract Metadata is
         if (prevMAHAX <= 20000 && currentMAHAX >= 25000) return true;
 
         return false;
+    }
+
+    /// @dev Returns current token URI metadata
+    /// @param _tokenId Token ID to fetch URI for.
+    function tokenURI(
+        uint256 _tokenId
+    ) external view override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "https://api.peopleofeden.com/token-uri/id-",
+                    _toString(_tokenId),
+                    "-history-",
+                    _toString(historyCount[_tokenId]),
+                    ".json"
+                )
+            );
+    }
+
+    function _toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT license
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 
     function _addData(uint256 nftId, TraitData memory data) internal {
