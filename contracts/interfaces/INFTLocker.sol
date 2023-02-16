ebfe1f1d88977df60d52199c88dd620d214c9f51
Steven Enamakel <enamakel@mahadao.com>
2023-02-16 16:46:57 +0530

    added contract for swapper

--- contracts/interfaces/INFTLocker.sol
+++ contracts/interfaces/INFTLocker.sol
@@ -2,18 +2,54 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
-
 interface INFTLocker {
-    function locked(uint256) external view returns (LockedBalance memory);
+    function createLockFor(
+        uint256 _value,
+        uint256 _lockDuration,
+        address _to,
+        bool _stakeNFT
+    ) external returns (uint256);
 
-    function increaseAmount(uint256 _tokenId, uint256 _value) external;
+    enum DepositType {
+        DEPOSIT_FOR_TYPE,
+        CREATE_LOCK_TYPE,
+        INCREASE_LOCK_AMOUNT,
+        INCREASE_UNLOCK_TIME,
+        MERGE_TYPE
+    }
 
-    function ownerOf(uint256 tokenId) external view returns (address owner);
+    struct Point {
+        int128 bias;
+        int128 slope; // # -dweight / dt
+        uint256 ts;
+        uint256 blk; // block
+    }
+
+    /* We cannot really do block numbers per se b/c slope is per time, not per block
+     * and per block could be fairly bad b/c Ethereum changes blocktimes.
+     * What we can do is to extrapolate ***At functions */
 
     struct LockedBalance {
         int128 amount;
         uint256 end;
         uint256 start;
     }
+
+    event Deposit(
+        address indexed provider,
+        uint256 tokenId,
+        uint256 value,
+        uint256 indexed locktime,
+        DepositType deposit_type,
+        uint256 ts
+    );
+
+    event Withdraw(
+        address indexed provider,
+        uint256 tokenId,
+        uint256 value,
+        uint256 ts
+    );
+
+    event Supply(uint256 prevSupply, uint256 supply);
 }
